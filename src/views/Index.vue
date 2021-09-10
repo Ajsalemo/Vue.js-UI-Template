eb83e5827e963e72e16b02d3d8e54774720933f0
Anthony Salemo <ajssalemo@gmail.com>
2021-09-10 11:37:32 -0400

    initial deployment

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -9,12 +9,13 @@
 <script>
 import Footer from "@/components/Footer";
 import Appbar from "@/components/Appbar";
-
+console.log(process.env.VUE_APP_APIURL);
+console.log(process.env.APIURL);
 export default {
   name: "Index",
   components: {
-      Footer,
-      Appbar,
-  }
+    Footer,
+    Appbar,
+  },
 };
 </script>
