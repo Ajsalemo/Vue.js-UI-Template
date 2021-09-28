8f049b821b6185bccd6b67bca61b4bc70ca51140
Anthony Salemo <ajssalemo@gmail.com>
2021-09-28 12:32:24 -0400

    adding env vars

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -9,6 +9,8 @@
 <script>
 import Footer from "@/components/Footer";
 import Appbar from "@/components/Appbar";
+
+console.log(`VUE_APP_LOCAL_GIT: ${process.env.VUE_APP_LOCAL_GIT}`)
 export default {
   name: "Index",
   components: {
