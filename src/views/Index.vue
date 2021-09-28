579192585a31728905bd6272423bb1f22fe5427e
Anthony Salemo <ajssalemo@gmail.com>
2021-09-28 13:46:12 -0400

    adding placeholders and removing console.log lines

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -10,8 +10,6 @@
 import Footer from "@/components/Footer";
 import Appbar from "@/components/Appbar";
 
-console.log(`VUE_APP_LOCAL_GIT: ${process.env.VUE_APP_LOCAL_GIT}`)
-console.log(`VUE_APP_DEV_OPS: ${process.env.VUE_APP_DEV_OPS}`)
 export default {
   name: "Index",
   components: {
