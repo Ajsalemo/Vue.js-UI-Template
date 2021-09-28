287afaf5199c00250eac4ebdd7d3ab77a5b0502d
Anthony Salemo <ajssalemo@gmail.com>
2021-09-28 12:50:16 -0400

    removing .env

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -11,6 +11,7 @@ import Footer from "@/components/Footer";
 import Appbar from "@/components/Appbar";
 
 console.log(`VUE_APP_LOCAL_GIT: ${process.env.VUE_APP_LOCAL_GIT}`)
+console.log(`VUE_APP_DEV_OPS: ${process.env.VUE_APP_DEV_OPS}`)
 export default {
   name: "Index",
   components: {
